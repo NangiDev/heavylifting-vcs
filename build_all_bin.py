0ca06ad5b0c3b00e060b287f1c2f1283dfe357a5
Joel Setterberg <nangi@pop-os.localdomain>
2022-11-07 16:11:05 +0100

    Created a build all binaries script

---
+++ build_all_bin.py
@@ -0,0 +1,22 @@
+#!/bin/python
+import os
+
+prev_dir = os.getcwd()
+
+def pushd(dir):
+    prev_dir = os.getcwd()
+    os.chdir(dir)
+
+def popd():
+    os.chdir(prev_dir)
+
+COL='\033[1;34m' # Blue Color
+NC='\033[0m' # No Color
+
+for next_dir in os.listdir("."):
+    if os.path.exists(os.path.join(next_dir, "src/main.rs")):
+        if os.path.exists(os.path.join(next_dir, "Cargo.toml")):
+            pushd(next_dir)
+            print("Module: " + COL + next_dir + NC)
+            os.system('cargo build')
+            popd()
